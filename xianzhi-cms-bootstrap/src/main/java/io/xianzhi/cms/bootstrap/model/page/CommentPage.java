e0c8d96bb540f13387c86f30ec24edc6fc8b41c2
Max <max@xianzhi.io>
2024-11-21 19:38:22 +0800

    add model

---
+++ xianzhi-cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/model/page/CommentPage.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2024 XianZhi Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.xianzhi.cms.bootstrap.model.page;
+
+import io.xianzhi.core.base.Page;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * Pagination query conditions for comment information.
+ * <p>
+ * This class extends the custom {@link Page} class to include pagination details and query conditions.
+ * </p>
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CommentPage extends Page implements Serializable {
+}
