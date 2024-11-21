cb302cb2f6858a46f90b8ecf22875ae889880309
Max <max@xianzhi.io>
2024-11-21 19:47:32 +0800

    add model

---
+++ xianzhi-cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/model/dto/FieldDTO.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 XianZhi Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.xianzhi.cms.bootstrap.model.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Represents the data transfer object for a field.
+ * <p>
+ * This class is used to transfer field-related information between different layers or components
+ * of the application, such as from the API layer to the service layer or vice versa.
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+@Data
+public class FieldDTO implements Serializable {
+}
