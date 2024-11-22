b33db67c16b648c2ce316b88ee5eb78c5c964208
Max <max@xianzhi.io>
2024-11-22 09:05:32 +0800

    add do

---
+++ xianzhi-cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/dao/dataobj/PayConfigDO.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2024 XianZhi Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.xianzhi.cms.bootstrap.dao.dataobj;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.xianzhi.mybatis.plus.base.BaseDO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Entity class for payConfig data.
+ * <p>
+ * This class maps to the `xz_pay_config` table in the database and represents
+ * the persistent data structure for payConfigs. It extends the {@link BaseDO}
+ * class to include common fields such as creation and modification timestamps.
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+@Data
+@TableName(value = "xz_pay_config")
+@EqualsAndHashCode(callSuper = true)
+public class PayConfigDO extends BaseDO {
+}
