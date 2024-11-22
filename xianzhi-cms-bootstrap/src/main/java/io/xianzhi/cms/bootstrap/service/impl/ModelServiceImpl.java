3b84113b4c268f57680e530b25b14a8c889360f0
Max <max@xianzhi.io>
2024-11-22 08:46:12 +0800

    add service impl

---
+++ xianzhi-cms-bootstrap/src/main/java/io/xianzhi/cms/bootstrap/service/impl/ModelServiceImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2024 XianZhi Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.xianzhi.cms.bootstrap.service.impl;
+
+import io.xianzhi.cms.bootstrap.service.ModelService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implementation of the model service interface.
+ * Handles business logic for model-related operations.
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ModelServiceImpl implements ModelService {
+}
