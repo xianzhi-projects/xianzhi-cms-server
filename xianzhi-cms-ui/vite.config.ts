713d56f6939c0112b78bca14e84c87875cd9e048
Max <max@xianzhi.io>
2024-11-22 10:36:10 +0800

    add ui

--- xianzhi-cms-ui/vite.config.ts
+++ xianzhi-cms-ui/vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
+import path from 'path'
 
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    script: {
+      defineModels: true
+    }
+  }),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),],
+
+
+  server: {
+    port: 3000,
+    open: true,
+    host: '0.0.0.0',
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8888/",
+        changeOrigin: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
+    }
+  }
+
 })
+
+
