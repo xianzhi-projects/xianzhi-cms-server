8779e1adcf8481e0621c41aebc13acd08a05ac03
Max <max@xianzhi.io>
2024-11-22 10:54:05 +0800

    add ui

--- xianzhi-cms-ui/src/stores/modules/router.ts
+++ xianzhi-cms-ui/src/stores/modules/router.ts
@@ -13,26 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
 
-import Antd from 'ant-design-vue';
-import App from './App.vue';
-import router from './router'
-import * as antIcons from '@ant-design/icons-vue'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.use(Antd)
-
-app.mount('#app')
-
-
-// 注册 ant-design 图标
-Object.keys(antIcons).forEach((key) => {
-  app.component(key, antIcons[key as keyof typeof antIcons])
-})
-
-app.config.globalProperties.$antIcons = antIcons
